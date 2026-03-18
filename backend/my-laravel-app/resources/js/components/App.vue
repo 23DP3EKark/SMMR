11af9392b1086a4862ddfdeb5758bd88aca50d59
23DP3EKark <A230457EK@rvt.lv>
2026-03-18 08:01:50 +0200

    Sāku taisīt lapas header sadaļu

--- backend/my-laravel-app/resources/js/components/App.vue
+++ backend/my-laravel-app/resources/js/components/App.vue
@@ -1,5 +1,17 @@
+<style>
+
+</style>
+
 <template>
     <div>
+        <Header />
+
         <h1>Welcome to Laravel 12 with Vue 3</h1>
+
+        <router-view></router-view>
     </div>
 </template>
+
+<script>
+    import Header from './Header.vue';
+</script>
---
+++ backend/my-laravel-app/resources/js/components/Header.vue
@@ -0,0 +1,27 @@
+<style scoped>
+    .off-screen-menu {
+        background-color: rgb(35, 35, 35);
+    }
+
+</style>
+
+<template>
+    <header>
+        <nav>
+            <ul class = "off-screen-menu">
+                <li><router-link to="/"><img src="" alt=""></router-link></li>
+                <li><router-link to="/quests">Darbi</router-link></li>
+                <li><router-link to="/travel">Ceļojumi</router-link></li>
+                <li><router-link to="/planner">Plānotājs</router-link></li>
+                <li><router-link to="/movies">Filmas</router-link></li>
+                <li><router-link to="/music">Mūzika</router-link></li>
+                <li><router-link to="/registration">Reģistrācija</router-link></li>
+                <li><router-link to="/login">Pieteikšanās</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+</script>
